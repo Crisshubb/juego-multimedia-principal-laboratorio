a50431cd92079b63de1ca28f574b5590c8195098
Crisshubb <186356852+Crisshubb@users.noreply.github.com>
2026-09-24 16:25:25 -0400

    feat(camera): recuperar objetivo y admitir desplazamiento de encuadre

--- My project/Assets/Scripts/Camara.cs
+++ My project/Assets/Scripts/Camara.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 public class Camara : MonoBehaviour
 {
     public Transform target;
+    [SerializeField] Vector2 desplazamiento;
+
+    void Awake()
+    {
+        if (target == null)
+        {
+            var jugador = FindFirstObjectByType<Jugador>();
+            if (jugador != null) target = jugador.transform;
+        }
+    }
 
     void LateUpdate()
     {
         if (target != null)
-            transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = new Vector3(
+                target.position.x + desplazamiento.x,
+                target.position.y + desplazamiento.y,
+                transform.position.z);
     }
 }
